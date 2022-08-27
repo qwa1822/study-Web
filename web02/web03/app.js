ad9d70defc4b7c091ec188bc84faee69d44438cc
qwa1822 <asdzxc1822@naver.com>
2022-08-27 16:37:17 +0900

    배열분해

---
+++ web02/web03/app.js
@@ -0,0 +1,9 @@
+
+
+const scores=[929321,899341,888336,777239,543671,243567];
+
+
+const highScore=scores[0];
+const secindHighscores=scores[1];
+
+const [gold,silver,bronze]=scores;
---
+++ web02/web03/two.js
@@ -0,0 +1,10 @@
+const cats=['Blue','Scout','Rocket'];
+const dogs=['Rusty','Wyatt'];
+
+const allPets=[...cats,...dogs]
+
+
+
+const nums=[13,4,5,6,3,3,1,2,7,8,9,10];
+Math.max(...nums)  //NAN
+Math.min(...nums) 
