924aff7829d00e39a980e776c66cd11da01c5a5c
qwa1822 <asdzxc1822@naver.com>
2022-08-27 18:27:53 +0900

    Coding 53

---
+++ Dom/3.js
@@ -0,0 +1,15 @@
+// const allLinks=document.querySelector('a');
+// for(let link of allLinks)
+// {
+//   link.innerText="all Link!";
+// }
+
+
+const h1=document.querySelector("h1");
+
+h1.innerText="SuminKim"
+
+
+const span=document.querySelector("span");
+
+span.innerText="Disgusting"
