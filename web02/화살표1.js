2d54c3a7da7de38c7bcd8255447dc0f0b4ef0cb4
qwa1822 <asdzxc1822@naver.com>
2022-08-27 10:24:10 +0900

    화살표

---
+++ web02/화살표1.js
@@ -0,0 +1,15 @@
+
+
+
+const rollDie=()=>{
+    Math.floor(Math.random*6)+1
+}
+
+
+const add=(a,b) =>{
+  return a+b;
+}
+
+const rollDIe=()=>(
+  Math.floor(Math.random*6)+1
+)
--- web02/화살표예제.js
+++ web02/화살표예제.js
@@ -1,4 +1,4 @@
 
 const greet=(x)=>{
-  return `Hey ${x}`
+  return `Hey ${x}!`
 }
