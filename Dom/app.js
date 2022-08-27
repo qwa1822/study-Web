4a041aa17b3125173bbb300f6258c77218e03bed
qwa1822 <asdzxc1822@naver.com>
2022-08-27 19:13:46 +0900

    coding55

---
+++ Dom/app.js
@@ -0,0 +1,13 @@
+
+
+
+const allLinks=document.querySelector('a');
+
+
+
+for(let link of allLinks)
+{
+  link.style.color='rgb(0,108,134)';
+  link.style.textDecorationColor='magenta';
+  link.style.textDecorationStyle='wavy'
+}
---
+++ Dom/coding55.js
@@ -0,0 +1,7 @@
+
+
+const div=document.getElementById("container");
+div.style.textAlign="center";
+
+const img=document.querySelector(img);
+img.style.width="150px";
