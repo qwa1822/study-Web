863d5959e231197854d49728c9ebf27e1aba36a6
qwa1822 <asdzxc1822@naver.com>
2022-08-27 10:47:59 +0900

    화살표함수 연습

---
+++ web02/화살표2.js
@@ -0,0 +1,12 @@
+console.log("Hello!!!");
+setTimeout(()=>{
+  console.log("dddd!!!");
+},3000)
+
+
+console.log("GOOD BYE!");
+
+const id=setInterval(()=>{
+  console.log(Math.random());
+},2000);
+
