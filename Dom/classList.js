6c6d51bd5bf5800f08e2197ba62ef3ace5834c57
qwa1822 <asdzxc1822@naver.com>
2022-08-28 15:37:05 +0900

    Coding 58

--- Dom/classList.js
+++ Dom/classList.js
@@ -1,8 +1,15 @@
 
 
-const lists=document.querySelectorAll('li');
 
 
-for(let list of lists){
-  list.classList.toggle("hightlight");
+const dd=document.querySelector("#container")
+
+
+
+
+for(let i=0; i<100; i++)
+{
+  const button=document.createElement("button");
+  button.append("Hello!");
+  dd.appendChild(button);
 }
