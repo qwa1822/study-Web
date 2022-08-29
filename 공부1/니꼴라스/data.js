480286a2aba088b9ba714b44cb53c1ba8e090df4
qwa1822 <asdzxc1822@naver.com>
2022-08-29 13:36:59 +0900

    계산기

---
+++ 공부1/니꼴라스/data.js
@@ -0,0 +1,28 @@
+
+
+const calculator={
+  add:function(a,b){
+    console.log(a+b);
+  },
+  minus:function(a,b)
+  {
+    console.log(a-b);
+  },
+  divide:function(a,b){
+    console.log(a/b);
+  },
+  multi:function(a,b)
+  {
+    console.log(a*b);
+  },
+  power:function(a,b)
+  {
+    console.log(a**b);
+  },
+};
+
+calculator.add(5,1)
+calculator.minus(3,2);
+calculator.divide(4,7);
+calculator.multi(5,2);
+calculator.power(2,8);
