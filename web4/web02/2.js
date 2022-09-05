deb0ad78afcf0bfba946955ef6629e612ed59c1d
qwa1822 <asdzxc1822@naver.com>
2022-09-06 00:43:41 +0900

    점수관리자 프로젝트

---
+++ web4/web02/2.js
@@ -0,0 +1,9 @@
+const input=document.querySelector("input");
+const h1=document.querySelector("h1");
+
+
+
+input.addEventListener('change',function(e)
+{
+  h1.innerText=`Welcome,${input.value}`;
+})
---
+++ web4/web02/app.js
@@ -0,0 +1,70 @@
+const p1={
+  score:0,
+  button:document.querySelector("#p1Button"),
+  display:document.querySelector("#p1Display"),
+}
+const p2={
+  score:0,
+  button:p2Button=document.querySelector("#p2Button"),
+  display:document.querySelector("#p2Display"),
+}
+
+
+const resetbutton=document.querySelector("#Reset");
+const winningScoreSelect=document.querySelector("#playto");
+
+
+
+let winningScore=5;
+
+let isGameOver=false;
+
+function updateScores(player,opponent){
+  if(!isGameOver){
+
+    player.score+=1;
+    if(player.score===winningScore){
+      isGameOver=true;
+      player.display.classList.add('has-text-success');
+      opponent.display.classList.add('has-text-danger');
+      player.disabled=true;
+      opponent.disabled=true;
+    }
+    player.display.textContent=player.score;
+
+  }
+}
+
+
+
+p1.button.addEventListener('click',function(){
+
+  updateScores(p1,p2)
+  
+}
+
+)
+
+p2.button.addEventListener('click',function(){
+  updateScores(p2,p1);
+})
+
+winningScoreSelect.addEventListener('change',function(){
+    winningScore=parseInt(this.value);
+    reset();
+})
+
+resetbutton.addEventListener('click',reset)
+
+
+function  reset(){
+  isGameOver=false;
+  for(let p of [p1,p2]){
+    p.score=0;
+    p.display.textContent=0;
+    p.display.classList.remove('has-text-success','has-text-danger');
+    p.button.disabled=false;
+  }
+  
+
+}
